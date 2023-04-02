10f9a1dc75de630951f8353f830207fdce0103fc
wideRuledAZ <danielmartinez0603@gmail.com>
2023-04-01 22:41:36 -0500

    Made my first popup its so cool

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import { Icon } from "leaflet";
 
 
 function App() {
+  const position = [36.14487659335152, -86.80265511885861]
   return (
     <div className="App">
       <header className="App-header">
-      <MapContainer center={[36.14487659335152, -86.80265511885861]} zoom={16}scrollWheelZoom={true}>
-      <TileLayer
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </MapContainer>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
       </header>
     </div>
   );
