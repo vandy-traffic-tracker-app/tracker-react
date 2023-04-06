6f5e29e0622e3e426384922f882e0bb6fc950c2f
wideRuledAZ <danielmartinez0603@gmail.com>
2023-04-06 16:16:59 -0500

    Added home icon

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -6,68 +6,64 @@ import './sidebar.css';
 
 function Sidebar(props) {
     const map = useMap();
-    // const homeIcon = new Icon({
-    //     iconUrl: require("../img/home-icon.png"),
-    //     iconSize: [38,38]
-    //   })
+
     const { markers } = props;
 
     console.log(markers)
 
-
-  const panels = [
+    const panels = [
     {
       id: "Home",
-      tab: 'Home',
+      tab: '<i style="font-size: large" class="fa fa-home"></i>',
       pane: "Welcome to VandyTracker",
       title: "About VandyTracker",
       position: "top",
     },
     {
       id: "Rothschild",
-      tab: '<i style="font-size: large" class="fa fa-user"></i> Rothschild Dining',
+      tab: 'Rothschild Dining',
       pane: "Rothschild Data",
       title: "Rothschild",
       position: "top",
     },
     {
       id: "Zeppos",
-      tab: '<i style="font-size: large" class="fa fa-gear"></i> Zeppos Dining',
+      tab: 'Zeppos Dining',
       pane: "Zeppos Data",
       title: "Zeppos",
       position: "top",
     },
     {
       id: "Rand-Dining-Center",
-      tab: '<i style="font-size: large" class="fa fa-github"></i> Rand Dining',
+      tab: 'Rand Dining',
       pane: "Rand Data",
       title: "Rand",
       position: "top",
     },
     {
       id: "EBI",
-      tab: '<i style="font-size: large" class="fa fa-github"></i> EBI Dining',
+      tab: 'EBI Dining',
       pane: "EBI Data",
       title: "EBI",
       position: "top",
     },
     {
       id: "Rec-Center",
-      tab: '<i style="font-size: large" class="fa fa-github"></i> Rec Center',
+      tab: 'Rec Center',
       pane: "Rec Center Data",
       title: "Rec Center",
       position: "top",
     },
     {
       id: "Alumni-Rec",
-      tab: '<i style="font-size: large" class="fa fa-github"></i> Alumni Gym',
+      tab: 'Alumni Gym',
       pane: "Alumni Gym Data",
       title: "Alumni Gym",
       position: "top",
     },
     {
       id: "Central-Library",
-      tab: '<i style="font-size: large" class="fa fa-github"></i> Central',
+      tab: 'Central',
       pane: "Central Library Data",
       title: "Central Library",
       position: "top",
