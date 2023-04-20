bae7e4cb820b1681870b5e6b19c2e266f6e41a72
Thu Nguyen <thunguyen@Thus-MacBook-Pro.local>
2023-04-19 19:13:47 -0500

    what would u do if u can

--- src/components/sidebar/newSb.js
+++ src/components/sidebar/newSb.js
@@ -9,12 +9,14 @@ import {
 import "./sidebar.css";
 import BarChart from "../barcharts/barchart";
 import locations from '../locations';
+import BarChartAvg from "../barcharts/avgbarchart";
 
 const Sidebar = (props) => {
 
   const {activeLocation, detailsClick} = props
   const [activeTab, setActiveTab] = useState(null);
   const [isPanelOpen, setIsPanelOpen] = useState(false);
+  const [date, setDate] = useState("Mon");
 
   const handleTabClick = (tab) => {
     console.log("tab click")
@@ -36,10 +38,31 @@ const Sidebar = (props) => {
   }, [detailsClick]);
 
   const FormPanel = ({activeTab}) => {
-    console.log("form panel " + activeTab + " loaded")
+    const id = activeTab
+    console.log("form panel " + {activeTab} + " loaded")
     return <div className="sidebar__panel-content">
-        This is the {activeTab} panel
+        <p1> {activeTab} </p1>
         <BarChart location={activeTab} />
+        <br />
+        <div>
+          <select className="selectButton" value={date} onChange={e =>setDate(e.target.value)}>
+            <option value="Mon">Mon</option>
+            <option value="Tues">Tues</option>
+            <option value="Wed">Wed</option>
+            <option value="Thurs">Thurs</option>
+            <option value="Fri">Fri</option>
+            <option value="Sat">Sat</option>
+            <option value="Sun">Sun</option>
+          </select>
+        </div>
+        <BarChartAvg location={activeTab} date={date} />
+        </div>;
+  };
+
+  const HomePanel = () => {
+    return <div className="sidebar__panel-content1">
+      <p1> Everyone at Vanderbilt University has their favorite spots on campus. They might love studying in Central Library, or love grabbing a sandwich at Rand between classes, or look forward to lifting weights at the Rec Center. Unfortunately, with over 13,537 students and over 4,500 staff members, these campus residents might not always be able to access these buildings due to overcrowding and long lines. To solve this issue, they might try to avoid common “rush hours”, but these are often variable per weekday and different for each location. 
+</p1>
         </div>;
   };
 
@@ -73,8 +96,8 @@ const Sidebar = (props) => {
         className="sidebar__panel"
         style={{ width: activeTab ? "60%" : "0%" }}
       >
-        {isPanelOpen && activeTab !== null && activeTab === "Home"}
-        {isPanelOpen && activeTab !== null && activeTab !== "Home" && <FormPanel activeTab={activeTab} />}
+        {isPanelOpen && activeTab !== null && activeTab === "home" && <HomePanel/>}
+        {isPanelOpen && activeTab !== null && activeTab !== "home" && <FormPanel activeTab={activeTab}/>}
       </div>
     </div>
   );
